1a175d26a2260aacf08734bb5dbcbddfee753241
meghaldonde <meghal.donde@gmail.com>
2020-08-04 14:00:33 -0400

    add simpson character images

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const simpsonsQuoteBtn = document.getElementById('simpson-quote');
+const imgSimpsonCharacter = document.getElementById('simpson-character');
 
-//custom server for CORS
+//custom server to avoid  CORS issue
 const proxyUrl = 'https://dry-cove-58467.herokuapp.com/';
 const apiUrl =
     'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
@@ -13,7 +14,7 @@ const simpsonsQuoteUrl = 'https://thesimpsonsquoteapi.glitch.me/quotes';
 
 
 const loader = document.getElementById('loader');
-var errorTimeOut = 0; //counter
+var errorTimeOut = 0;
 
 function showLoadingSpinner() {
     loader.hidden = false;
@@ -29,12 +30,22 @@ function removeLoadingSpinner() {
     }
 }
 
+function showCharacterImage(imageUrl) {
+    if (imageUrl) {
+        imgSimpsonCharacter.hidden = false;
+        imgSimpsonCharacter.src = imageUrl;
+    } else {
+        imgSimpsonCharacter.hidden = true;
+    }
+}
 
+function hideCharacterImage() {
+    imgSimpsonCharacter.hidden = true;
+}
 async function getQuote(quoteType) {
 
-
-
-    showLoadingSpinner()
+    showLoadingSpinner();
+    hideCharacterImage();
 
     try {
 
@@ -43,17 +54,19 @@ async function getQuote(quoteType) {
         if (quoteType === 'simpsons quote') {
             url = simpsonsQuoteUrl;
 
+
         }
 
         const response = await fetch(url);
         const data = await response.json();
+
         //check if simpsons quote requested
         if (quoteType === 'simpsons quote') {
-            authorText.innerText = "Character - " + data[0].character;
+            authorText.innerText = data[0].character;
             quoteText.innerText = data[0].quote;
+            showCharacterImage(data[0].image);
 
-            console.log(data[0]);
-        } else { //default quote 
+        } else { //default quote API
             //If response has no author , reset to 'unknown'
             if (data.quoteAuthor === '') {
                 data.quoteAuthor = 'Unknown';
@@ -77,8 +90,9 @@ async function getQuote(quoteType) {
 
 
     } catch (error) {
-        //counter to avoid infinite recursive function call
-        if (errorTimeOut <= 20) {
+        //counter to avoid infinite recursive function call threshhold set to 30
+        const errorThreshhold = 50;
+        if (errorTimeOut <= errorThreshhold) {
             getQuote();
 
         } else {
